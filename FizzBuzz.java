5338610a01f03a4418d59b26fbf2940f4af23db0
Ankit <tomarankit78@gmail.com>
2025-02-05 02:35:32 +0530

    Updated files with input and output

---
+++ FizzBuzz.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class FizzBuzz {
+
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+
+        int n=scn.nextInt();
+
+        if(n%3==0 && n%5==0){
+            System.out.println("FizzBuzz");
+        }else if(n%3==0){
+            System.out.println("Fizz");
+        }else if(n%5==0){
+            System.out.print("Buzz");
+        }
+            scn.close();
+    }
+    
+}
+
+/*
+ 3 -- fizz
+ 5 -- buzz
+
+ 15 --FizzBuzz
+ 
+ */

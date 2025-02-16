fefd125a68902c4c8543f95b23c4f3ff138c62fb
Ankit <tomarankit78@gmail.com>
2025-02-17 01:36:35 +0530

    Updated Programs with Input and Output

--- ReversetheArray.java
+++ ReversetheArray.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 
 public class ReversetheArray {
     public static void main(String args[]){
-        int arr[]={1,2,3,4,5,6,7,8,9};
+       // int arr[]={1,2,3,4,5,6,7,8,9};
+
+        int arr[]={-3,-2,6,8,4,8,5};
+     //   [5, 8, 4, 8, 6, -2, -3]
+     
             int temp=0;
             int i=0;
             int j=arr.length-1;

5c588c83d1843ee8ce7e1692ebed9107cbd5b51a
Ankit <tomarankit78@gmail.com>
2025-02-27 11:13:34 +0530

    New Programs of Arrays with Examples

--- AGpairOptimized.java
+++ AGpairOptimized.java
@@ -3,15 +3,17 @@ public class AGpairOptimized {
 
         String s ="'b','c','a','g','g','a','a','g'";
        // String s1="bcaggaag";
+       // String s2="acgdgag";
+       // String s3="abegag";
 
-        int count=0;        int ans=0;
+        int countOFa=0;        int ans=0; // number of g that can form pair
 
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='a'){
-                count++;
+                countOFa++;
             }
             if(s.charAt(i)=='g'){
-               ans=ans+count;
+               ans=ans+countOFa;  // this g can form the pair of.
             }
         }
             System.out.println("Pair of AG count optimized : "+ans);
@@ -19,3 +21,7 @@ public class AGpairOptimized {
     }
     
 }
+/*
+
+ Pair of AG count optimized : 5
+ */
---
+++ LeadersInaArray.java
@@ -0,0 +1,18 @@
+public class LeadersInaArray {
+    public static void main(String args[]){
+        int ans=0;
+        int arr[]={8,-2,7,6,5,1};
+
+        for(int i=0;i<arr.length;i++){
+            boolean flag=true;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>=arr[i]){
+                    flag=false;
+                    break;
+                }
+            }
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}

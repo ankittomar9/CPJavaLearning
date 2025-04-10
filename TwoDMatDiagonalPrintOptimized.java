83f4f9a76eb21e44528cc3fead13ee498c9d3046
Ankit <tomarankit78@gmail.com>
2025-04-10 10:32:53 +0530

    New Array Programs with Input and Output

---
+++ TwoDMatDiagonalPrintOptimized.java
@@ -0,0 +1,15 @@
+public class TwoDMatDiagonalPrintOptimized {
+    public static void main(String args[]){
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i][i]);
+        }
+    }
+
+
+
+
+
+
+}

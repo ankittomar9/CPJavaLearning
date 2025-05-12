2599e5acf623a0fe8c6bca9b6641574fb1f72dd1
Ankit <tomarankit78@gmail.com>
2025-05-12 16:47:35 +0530

    [Topic] IF-ELSE  [Problems]

--- DaysInAMonth.java
+++ DaysInAMonth.java
@@ -51,3 +51,31 @@ public class DaysInAMonth {
         }
     }
 
+
+    /*/
+  Problem Description
+You are given an integer A.
+You have to tell how many days are there in the month denoted by A in a non-leap year.
+
+Months are denoted as follows:
+January : 1
+February : 2
+March : 3
+April : 4
+May : 5
+June : 6
+July : 7
+August : 8
+September : 9
+October : 10
+November : 11
+December : 12
+
+Problem Constraints
+1 <= A <= 12
+
+Input Format
+The input contains a single integer A.
+Output Format
+Print a single integer denoting the number of days on a single line
+    */
---
+++ MinOfThree.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class MinOfThree {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        int c=scn.nextInt();
+        
+        MinOfThreeFunc(a,b,c);
+    }
+
+    public static void MinOfThreeFunc(int x,int y,int z){
+
+        if(x<=y && x<=z){
+            System.out.println("x is smaller "+x);
+        }
+        else  if(y<=x && y<=z){
+            System.out.println("y is smaller "+y);
+        }
+         else  if(z<=x && z<=y){
+            System.out.println("z is smaller "+z);
+        }
+        else 
+         System.out.println("Invalid input ");
+        
+    }
+    
+}
+
+
+/*
+ * Write a program to input three numbers(A, B & C) from user and print the minimum element among A, B & C.
+
+Problem Constraints
+
+
+1 <= A <= 1000000
+
+1 <= B <= 1000000
+
+1 <= C <= 1000000
+
+
+
+Input Format
+
+
+First line is a single integer A.
+Second line is a single integer B.
+Third line is a single integer C.
+
+
+ */

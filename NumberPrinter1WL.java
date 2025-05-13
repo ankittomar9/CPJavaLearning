9879652b63b19cd22940f5d072acc5fff5a59fe2
Ankit <tomarankit78@gmail.com>
2025-05-13 18:31:31 +0530

    [Topic] While-Loop [Problems]

--- NumberPrinter1WL.java
+++ NumberPrinter1WL.java
@@ -12,9 +12,34 @@ public class NumberPrinter1WL {
     public static void NumberPrinter1(int n){
         int i=0;
         while (i<=n){
-            System.out.println(i);
+            System.out.print(" "+i);
             i++;
         }
     }
     
 }
+
+/*
+ Write a program that takes a positive integer N as input from the user and prints all natural numbers from 1 to N, with each number followed by a space (including the last number).
+Problem Constraints
+1 <= N <= 1000000
+
+Input Format
+A single line representing N
+Output Format
+N space separated integers from 1 to N, with each number followed by a space, including the last number.
+
+Example Input
+Input 1:
+5
+Input 2:
+
+10
+Example Output
+
+Output 1:
+
+1 2 3 4 5 
+Output 2:
+1 2 3 4 5 6 7 8 9 10 
+ */
--- NumberPrinter2WL.java
+++ NumberPrinter2WL.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class NumberPrinter2WL {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        NumberPrinter2Func(a);
+
+    }
+    public static void NumberPrinter2Func(int x){
+        int i=0;
+        while(x>=i){
+            System.out.print(" " +x);
+            x--;
+        }
+    }
     
 }
+
+/*
+ * 
+ Write a program to print all Natural numbers from N to 1 where you have to take N as input from user
+ 1 <= N <= 10000000
+Input Format
+A single line representing N
+
+Output Format
+N space separated integers from N to 1.
+
+Example Input
+Input 1:
+5
+ 
+5 4 3 2 1
+ */

000f9f338f21634bd3660694b8ab5c200888d5c0
Ankit <tomarankit78@gmail.com>
2025-05-13 19:06:50 +0530

    [Topic] While-Loop [Problems]

--- MultiplesOfANumber.java
+++ MultiplesOfANumber.java
@@ -12,7 +12,7 @@ public class MultiplesOfANumber {
     }
 
     public static void MultiplesOfANumberFunc(int a){
-        int i=0;
+        int i=1;
         while(i<=a){
             if(i%4==0){
                 System.out.print(" "+i);
@@ -25,3 +25,23 @@ public class MultiplesOfANumber {
 
     
 }
+
+/*Problem Description
+Given an integer input N, print all multiples of 4 less than or equal to N.
+Problem Constraints
+1 <= N <= 10000
+Input Format
+Single line containing an integer N.
+Output Format
+Space separated integers representing multiples of 4 less than or equal to N.
+Examle Input
+22
+Example Output
+4 8 12 16 20
+Example Explanation
+1 * 4 = 4
+2 * 4 = 8
+3 * 4 = 12
+4 * 4 = 16
+5 * 4 = 20
+All are multiples of 4 less than 22 */

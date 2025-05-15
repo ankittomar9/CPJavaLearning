b4bc3355cecf481adba7a5453f722eaf1292bb0f
Ankit <tomarankit78@gmail.com>
2025-05-15 13:20:28 +0530

    [Topic] While-Loop [Problems]

--- CountTheDigits.java
+++ CountTheDigits.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class CountTheDigits {
     public static void main(String args[]){
         Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+
+        CountTheDigitsFunc(x);
+
     }
-    
+    public static void CountTheDigitsFunc(int n){
+    int count=0;
+    while(n>0){
+        //n=n%10;
+        n=n/10;
+        count++;
+    }
+       System.out.println(count);
 }
+}
+
+/* */

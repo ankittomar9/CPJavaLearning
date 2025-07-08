19f35c987338eeb910e858165ba17eb63f5196b9
Ankit <tomarankit78@gmail.com>
2025-07-08 23:45:55 +0530

    New practicePrograms

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -7,6 +7,10 @@ public class PrimeNumberCheck {
         PrimeNumberCheckFunc(x);
     }
     public static void PrimeNumberCheckFunc(int x){
+        if(x==0){
+              System.out.println("Not prime  ");
+              return;
+        }
         int i=1;int count=0;
         while(i<=x){
             if(x%i==0){

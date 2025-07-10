1371e6986fa640ba7562ffde20abf81dedb2c952
Ankit <tomarankit78@gmail.com>
2025-07-10 20:02:51 +0530

    New Practice problems Added

--- ArraysCountPairs.java
+++ ArraysCountPairs.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class ArraysCountPairs {
     public static void main(String args[]){
          int arr[]={2,5,1,4,8,0,8,1,3,8};
+         Scanner scn=new Scanner(System.in);
+         int x=scn.nextInt();
+         int count=0;
 
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+           
+                if(arr[i]+arr[j]==x)                
+                    count++;
+                  
+            }
+        }
+            System.out.println(" "+count);
 
     }
+
+
     
 }
 
---
+++ ReverseArray.java
@@ -0,0 +1,23 @@
+public class ReverseArray {
+    public static void main(String args[]){
+         int arr[]={2,5,1,4,8,0,8,1,3,8};
+
+       //  int i=arr[0];int j=arr[arr.length-1];
+       int i=0; int j=arr.length-1;  
+       while(i<j){
+
+        int temp=arr[i];
+       arr[i]=arr[j];
+       arr[j]=temp;
+
+            i++;
+            j--;
+         }
+       //  System.out.println(.toString(arr));
+
+       for(int k=0;k<arr.length;k++){
+        System.out.print(" "+arr[k]);
+       }
+
+    }
+}

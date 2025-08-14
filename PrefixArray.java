459818407c2d5db4010edb8971855574505bb413
Ankit <tomarankit78@gmail.com>
2025-08-14 17:49:57 +0530

    New Practice programs

--- PrefixArray.java
+++ PrefixArray.java
@@ -4,5 +4,26 @@ class PrefixArray{
 public static void main(String args[]){
      int[] arr1={[8, 3, 1, 8, 0, 8, 4, 1, 5, 2]}; 
 
+     //pf[i] =sum(0-i)=pf[i-1]+A[i]
+
+        int n=arr1.length; int sum=0;
+        int prefix_arr[]=new int[n];
+
+
+        
+    int n=arr1.length; int sum=0;
+    int prefix_arr[]=new int[n]; 
+    
+    for(int i=0;i<n;i++){
+        sum=sum+arr1[i];
+        prefix_arr[i]=sum;
+    }
+    System.out.println(Arrays.toString(arr1));  // SC O(N) original array modified 
+  //  System.out.println(Arrays.toString(prefix_arr)); SC // O(N) original array intact
+    //[2, 5, 9, 17, 23, 30, 35]
 }
 }
+
+
+
+

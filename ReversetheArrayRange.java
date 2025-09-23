443b655cb991f1e8665a25307a8ef2321c6d945c
tomar <tomarankit78@gmail.com>
2025-09-23 17:12:21 +0530

    New Practice programs

---
+++ ReversetheArrayRange.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ReversetheArrayRange {
+    public static void main(String args[]){
+        int arr[]={2,5,1,4,8,0,8,1,3,8};
+        int l=2;int r=6; // range from where the array needs to be reversed
+
+        while(l<r){
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;r--;
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+    
+}

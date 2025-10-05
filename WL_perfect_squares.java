bb469b6de955b650d33d02c9916b1be8ec7f6909
tomar <tomarankit78@gmail.com>
2025-10-05 21:43:38 +0530

    While Loop program [Revision]

---
+++ WL_perfect_squares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WL_perfect_squares {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+
+            WL_perfect_squares_func(x);
+    }
+    public static void WL_perfect_squares_func( int a){
+        int i=1;
+        while(i<=a){
+            if()
+            
+            
+            
+            i++;}
+
+     }
+}

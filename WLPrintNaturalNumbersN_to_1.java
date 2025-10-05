7462f73fcda8523c8a1b5df70a1f2049135a6ce3
tomar <tomarankit78@gmail.com>
2025-10-05 18:46:16 +0530

    While Loop program [Revision]

--- WLPrintNaturalNumbersN_to_1.java
+++ WLPrintNaturalNumbersN_to_1.java
@@ -4,17 +4,21 @@ public class WLPrintNaturalNumbersN_to_1 {
     
     public static void main(String args[]){
         Scanner scn=new Scanner(System.in);
-        int x=scn.nextInt();
+        int x=scn.nextInt();  // 10
         
         WLPrintNaturalNumbers1_toN_Func( x);
     }
     public static void WLPrintNaturalNumbers1_toN_Func(int a){
-        int i=1;
-        while(i<a){   // 1>10   //10>1  // 1<10 
-            System.out.println(" "+i);
+        int i=a; int j=1;
+        while(i>=j){   // 1>10   //10>1  // 1<10 
+            System.out.print(" "+i);
           i--;  
         }
 
     }
 
 }
+
+/*
+Write a program to print all Natural numbers from N to 1 where you have to take N as input from user
+ */

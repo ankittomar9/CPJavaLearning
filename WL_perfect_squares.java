a81cf4f2ccbaa7e5731501056f3db2cc92c8b487
tomar <tomarankit78@gmail.com>
2025-10-05 23:36:16 +0530

    While Loop program [Revision]

--- WL_perfect_squares.java
+++ WL_perfect_squares.java
@@ -9,15 +9,19 @@ public class WL_perfect_squares {
     }
     public static void WL_perfect_squares_func( int a){
         int i=1;
-        while(i<=a){
-            if(i*i==a){
-                System.out.print(" "+i);
-            }
+        while(i*i<=a){
            
-            
-            
+                System.out.print(i*i+" ");   
             i++;
         }
 
      }
 }
+
+/*
+ * 
+Take an input of a number A from the user. Print all perfect squares less than or equal to A.
+Note - Perfect squares are integers whose square root is an integer. (For Example: 16 is perfect square as √16 = 4, or 42 = 16)
+
+
+ */

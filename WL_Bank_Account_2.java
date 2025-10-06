aafadce37b969ee86fe04e4eaf8975bb4f640a07
tomar <tomarankit78@gmail.com>
2025-10-07 01:09:46 +0530

    While Loop program [Revision]

---
+++ WL_Bank_Account_2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/*Revise this again */
+
+public class WL_Bank_Account_2 {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+
+        long initial_balance=scn.nextLong();  //N
+        long number_of_operations=scn.nextLong();  //M
+        long new_balance=0;
+
+
+        while(number_of_operations>0){
+            int type=scn.nextInt();
+             long value=scn.nextLong();  //M
+
+                if(type==1){
+                    new_balance=initial_balance+value;
+                     System.out.println(new_balance);
+                }
+                else if (type == 2) {
+                        if (value > initial_balance) {
+                            // Insufficient Funds: Print message and SKIP the transaction.
+                            System.out.println("Insufficient Funds");
+                        } else {
+                            // Sufficient Funds: Perform the calculation and UPDATE the balance.
+                            initial_balance = initial_balance - value;
+                            System.out.println(initial_balance);
+                        }
+                    }
+
+
+
+
+            number_of_operations--;
+        }
+        
+        
+
+
+
+    }
+
+}
---
+++ WL_Palindromic_Integer.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WL_Palindromic_Integer {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        WL_Palindromic_Integer_func(n);
+    }
+
+    public static void WL_Palindromic_Integer_func(int a){
+        int result=0; int original_a=a;
+       
+        while(a>0){
+             int digit=a%10; // 343  -> 343%10= 34  3  
+             result=(result*10)+digit;
+            a=a/10;
+            
+        }
+        // System.out.println(result);
+        if(result==original_a){
+                 System.out.println("given number is palindromic : " );
+        }
+       else {
+         System.out.println("given number is not palindromic : " );
+       }
+
+    } 
+    
+}

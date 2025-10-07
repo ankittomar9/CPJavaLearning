51522bf1a36e3cd5ab57b2a6754dd962b7d94fca
tomar <tomarankit78@gmail.com>
2025-10-07 18:12:13 +0530

    FOR Loop program [Revision]

---
+++ FOR_L_Perfect_Number.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class FOR_L_Perfect_Number {
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        int t=scn.nextInt();
+
+        for(int j=1;j<=t;j++){
+
+        int n=scn.nextInt();
+        int sum=0;
+
+        for(int i=1;i<=n-1;i++){
+            if(n%i==0){
+                sum=sum+i;
+                
+            }
+        }
+        if(n==sum){
+                 System.out.println("Given number is perfect number");
+        }else{
+              System.out.println("Given number is NOT perfect number");
+        }
+
+    }
+}
+    
+}
+
+
+/*
+Given the Number of Test Cases as T,
+For each test case, take an integer N as input, you have to tell whether it is a perfect number or not.
+
+A perfect number is a positive integer that is equal to the sum of its proper positive divisors (excluding the number itself). 
+A positive proper divisor divides a number without leaving any remainder.
+ */
---
+++ FOR_L_prime_number.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class FOR_L_prime_number {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        FOR_L_prime_number_Func(n);
+
+    }
+
+    public static void FOR_L_prime_number_Func(int a){
+        // if(a==0){
+        //     System.out.print("Not Prime nor anything");
+        // }
+        int count=0;
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.print("Given number is Prime number");
+        }else{
+            System.out.print("Given number is Not a Prime number");
+        }
+        
+    }
+}
+
+/*
+Take an integer A as input, you have to tell whether it is a prime number or not.
+
+A prime number is a natural number greater than 1 which is divisible only by 1 and itself.
+ */

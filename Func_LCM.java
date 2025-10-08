8f52ffdafe7f308edafb5b74a3162ec99473cc70
tomar <tomarankit78@gmail.com>
2025-10-08 21:38:58 +0530

    Function programs [Revision]

---
+++ Func_LCM.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Func_LCM {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();    int m=scn.nextInt();    
+        Func_LCM_Func(n,m);
+    }
+    public static void Func_LCM_Func(int n,int m){
+
+    int mn=Math.min(n, m);
+        int mx=Math.max(n, m);
+     for(int i=mn;i<=mx;i++){
+
+        
+    }
+
+    }
+    
+}

1caed561a4899dff9d3f2e92d5fa3e70eced156d
tomar <tomarankit78@gmail.com>
2025-10-08 12:42:32 +0530

    Function programs [Revision]

--- Func_VolumeOfSphere.java
+++ Func_VolumeOfSphere.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Func_VolumeOfSphere {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        double n=scn.nextDouble();
+
+        Func_VolumeOfSphereFunc(n);
+    }
+    public static  void Func_VolumeOfSphereFunc(double a){
+        double vol=0;
+
+        vol=(Math.PI *a*a*a)*4/3;
+
+        System.out.println(vol);
+
+        System.out.println(Math.ceil(vol));
+          System.out.println(Math.round(vol));
+            System.out.println(Math.floor(vol));
+        
+
+
+    }
     
 }

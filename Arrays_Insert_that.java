7eeb075148b87d936d563e221b59efc6d99a27ae
tomar <tomarankit78@gmail.com>
2025-10-09 20:28:54 +0530

    Arrays programs [Revision]

---
+++ Arrays_Insert_that.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Arrays_Insert_that {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        
+            
+
+    }
+    
+    
+}

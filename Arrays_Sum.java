28cffe63ee86f1533bcbae27c89af7a59007892d
tomar <tomarankit78@gmail.com>
2025-10-09 22:25:57 +0530

    Arrays programs [Revision]

--- Arrays_Sum.java
+++ Arrays_Sum.java
@@ -8,7 +8,8 @@ public class Arrays_Sum {
         int sum=0;        
             for(int i=0;i<=arr.length-1;i++){
                 //sum=sum+arr[i];
-               System.out.println(    sum=sum+i);
+                        System.out.println(    sum=sum+i);
+             //  System.out.println(    sum=sum+i);
             }
 
             System.out.println("Sum of all the elements in the array : " +sum);

fbcac5f745270dea9f1844196b4b91f628533af7
tomar <tomarankit78@gmail.com>
2025-10-09 18:44:22 +0530

    Arrays programs [Revision]

---
+++ Arrays_PrintReverse.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arrays_PrintReverse {
+    public static void main(String args[]){
+        int arr[]={5 ,1 ,2 ,3 ,4 ,5};
+
+        for(int i=arr.length-1;i>=0;i++){
+            System.out.print(Arrays.toString(arr));
+        }
+        
+    }
+    
+}

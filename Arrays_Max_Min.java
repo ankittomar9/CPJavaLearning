136352785f8e813e4a8580a7d69e8662e45a80bb
tomar <tomarankit78@gmail.com>
2025-10-09 22:31:52 +0530

    Arrays programs [Revision]

--- Arrays_Max_Min.java
+++ Arrays_Max_Min.java
@@ -2,7 +2,11 @@ public class Arrays_Max_Min {
     public static void main(String args[]){
         int arr[]={2,9,6,7,8,2,4,5,1,3,6};
 
-        
+            int min=Integer.MAX_VALUE;
+            int max=Integer.MIN_VALUE;
+
+
+            
     }
     
 }

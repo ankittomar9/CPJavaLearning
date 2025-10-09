b70fb07d86162ce856876f137f9edca9d25a81c8
tomar <tomarankit78@gmail.com>
2025-10-09 22:25:48 +0530

    Arrays programs [Revision]

---
+++ Arrays_Sum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Arrays_Sum {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int arr[]={9,8,7,6,5,4,3,2,1};
+        // int arr[]={2,9,6,7,8,2,4,5,1,3,6,2,1,6,9,3,4,6,1,2,6};
+        int sum=0;        
+            for(int i=0;i<=arr.length-1;i++){
+                //sum=sum+arr[i];
+               System.out.println(    sum=sum+i);
+            }
+
+            System.out.println("Sum of all the elements in the array : " +sum);
+    }
+    
+}
+
+
+/*
+ * 
+ * 
+Sum of all the elements in the array : 
+ * 
+ */

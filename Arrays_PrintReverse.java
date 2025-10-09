da12466aa228d1b8758ef814356d06e898bb4d90
tomar <tomarankit78@gmail.com>
2025-10-09 19:14:08 +0530

    Arrays programs [Revision]

--- Arrays_PrintReverse.java
+++ Arrays_PrintReverse.java
@@ -2,12 +2,21 @@ import java.util.Arrays;
 
 public class Arrays_PrintReverse {
     public static void main(String args[]){
-        int arr[]={5 ,1 ,2 ,3 ,4 ,5};
+        int arr[]={1 ,2 ,3 ,4 ,5};
 
-        for(int i=arr.length-1;i>=0;i++){
-            System.out.print(Arrays.toString(arr));
-        }
-        
+        for(int i=arr.length-1;i>=0;i--){
+              System.out.print(" "+arr[i]);
+                  }
+
+
+                   System.out.println("\nOriginal Array State: ");
+        System.out.println(Arrays.toString(arr));
+       //  System.out.print(Arrays.toString(arr));
     }
     
 }
+
+
+/*
+
+ */

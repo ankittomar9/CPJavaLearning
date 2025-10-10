f615b0593461c9b50dbb8b9d4c86e027ae8d4e72
tomar <tomarankit78@gmail.com>
2025-10-10 23:45:37 +0530

    Arrays programs [Revision]

--- Arrays_Consecutive_Duplicate.java
+++ Arrays_Consecutive_Duplicate.java
@@ -4,9 +4,7 @@ public class Arrays_Consecutive_Duplicate {
     public static void main(String args[]){
         int arr[]={1,2,3,3};
       //     int arr2[]={1,2,5,1,5,1};
-
         boolean isDuplicate=false;
-
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]==arr[i+1]){
                 isDuplicate=true;
@@ -20,10 +18,6 @@ public class Arrays_Consecutive_Duplicate {
 
     }
 }
-
-
 /*
- * 
- * 
- * 
+ to check if the given array A has consecutive duplicate elements or not.
  */

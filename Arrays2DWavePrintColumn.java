414a5f3f453f74b2ac713c3e9c40e92675817cbe
tomar <tomarankit78@gmail.com>
2025-10-13 13:01:27 +0530

    Arrays programs [Revision]

---
+++ Arrays2DWavePrintColumn.java
@@ -0,0 +1,35 @@
+public class Arrays2DWavePrintColumn {
+    
+    public static void main(String args[]){
+        int arr[][]={{4,1,2},{7,4,4},{3,7,4}}; //expected 4 7 3 7 4 1 2 4 4 
+        int n=arr.length;
+        for(int j=0;j<n;j++){
+            if(j%2==0){
+                for(int i=0;i<n;i++){
+                System.out.print(arr[i][j]+" ");
+               }
+            }
+            else{
+                for (int i = n - 1; i >= 0; i--){
+                     System.out.print(arr[i][j]+" ");
+                }
+            }
+            
+
+        }
+
+    }
+
+}
+
+
+/*
+ * 4 1 2
+ * 7 4 4
+ * 3 7 4
+ * 
+ * output
+ * 4 7 3
+ * 7 4 1
+ * 2 4 4
+ */

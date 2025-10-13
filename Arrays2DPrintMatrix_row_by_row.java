e0e3bd46d5157058d357371c282542dd44cc49da
tomar <tomarankit78@gmail.com>
2025-10-13 12:05:11 +0530

    Arrays programs [Revision]

--- Arrays2DPrintMatrix_row_by_row.java
+++ Arrays2DPrintMatrix_row_by_row.java
@@ -1,6 +1,19 @@
 public class Arrays2DPrintMatrix_row_by_row {
     public static void main(String args[]){
+        int arr[][]={{7,8,9,0},{4,5,6,7},{1,2,3,4}};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                   System.out.print(arr[i][j]+" ");
+            }
+             System.out.println("");
         
     }
     
+    }
 }
+
+/*
+ * 
+ * 
+ */

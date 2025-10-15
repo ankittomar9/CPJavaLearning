ee08c80102665c445cd0e0e448a1727351871d3e
tomar <tomarankit78@gmail.com>
2025-10-16 00:42:11 +0530

    ArrayList Programs

--- ArrayList_Printing.java
+++ ArrayList_Printing.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayList_Printing {
 
     public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
         
         ArrayList<Integer> arr=new ArrayList<>();
+         ArrayList<Integer> arr2=new ArrayList<>();
+        int count=scn.nextInt();
 
         arr.add(10);
         arr.add(20);
@@ -17,7 +21,13 @@ public class ArrayList_Printing {
 
         System.out.println(arr);
 
+         for(int i=0;i<count;i++){
+            int value=scn.nextInt();
+            arr2.add(value);
+         //   System.out.println(arr);
 
+        }
+          System.out.println(arr2);
 
     }
     

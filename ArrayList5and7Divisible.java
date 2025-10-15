238154cf5727af758babc465264411db1872b0b6
tomar <tomarankit78@gmail.com>
2025-10-15 23:54:34 +0530

    ArrayList Programs

--- ArrayList5and7Divisible.java
+++ ArrayList5and7Divisible.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 public class ArrayList5and7Divisible {
     public static void main(String args[]){
-        List<Integer>arr=new ArrayList<>();
+    //    List<Integer>arr=new ArrayList<>();
          List<Integer>arr1=new ArrayList<>();
+          List<Integer> arr = List.of(5, 7, 70, 50, 35); 
         
-        arr.add(5);
-         arr.add(7);
-          arr.add(70);
-           arr.add(50);    
-            arr.add(35);    
+        // arr.add(5);
+        //  arr.add(7);
+        //   arr.add(70);
+        //    arr.add(50);    
+        //     arr.add(35);    
 
             for(int i=0;i<arr.size();i++){
                 int val=arr.get(i);

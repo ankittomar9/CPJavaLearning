88ee4e09e4ca4ba01874a541cd5c3e5dfe0b95ed
tomar <tomarankit78@gmail.com>
2025-10-15 23:33:18 +0530

    ArrayList Programs

--- ArrayList_Printing.java
+++ ArrayList_Printing.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayList_Printing {
 
     public static void main(String args[]){
-
         
+        ArrayList<Integer> arr=new ArrayList<>();
+
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        for(int i=0;i<arr.length;i++){
+            
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
 
     }
     

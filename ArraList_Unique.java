6e91df1bb895a25bc0ac00fd4d74414fb6578b86
tomar <tomarankit78@gmail.com>
2025-10-18 11:41:14 +0530

    ArrayList Programs

---
+++ ArraList_Unique.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraList_Unique {
+    public static void main(String args[]){
+     //  List<Integer> list=List.of(2 ,4,6,8,1,2,4,6,7,1);
+       ArrayList<Integer>list=new ArrayList<>(List.of(2 ,4,6,8,1,2,4,6,7,1));
+
+      //Sorting it 
+      Collections.sort(list); 
+      System.out.println(list);
+
+        
+    }
+    
+}
+
+
+/*You are given an array A of N elements. You have to make all elements unique.
+To do so, in one step you can increase any number by one.
+Find the minimum number of steps. */

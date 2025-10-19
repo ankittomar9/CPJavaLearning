7541a1419b71ada349755e0ba7ff030d2d6a69d6
tomar <tomarankit78@gmail.com>
2025-10-19 23:53:37 +0530

    ArrayList Programs

--- ArrayListOperations.java
+++ ArrayListOperations.java
@@ -6,7 +6,7 @@ public class ArrayListOperations {
 
     public static void main(String args[]){
         Scanner scn=new Scanner(System.in);
-        List<Integer> list=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();  // 10 20 30 40 50 60 70 
 
         for(int i=0;i<list.size();i++){
             

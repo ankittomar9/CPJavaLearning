8af53da37c8945fee2e74dccaba0b1bda9d367d1
tomar <tomarankit78@gmail.com>
2025-10-20 00:03:26 +0530

    ArrayList Programs

--- ArrayListOperations.java
+++ ArrayListOperations.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class ArrayListOperations {
-
     public static void main(String args[]){
+
         Scanner scn=new Scanner(System.in);
         int x=scn.nextInt(); //arrrayList size;
         List<Integer> list=new ArrayList<>();  // Empty ArrayList
 
         for(int i=0;i<x;i++){
            // int element=scn.nextInt();
-            
-           list.add(scn.nextInt()); 
-           
+           list.add(scn.nextInt());      
         }
-
         System.out.println(list);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
     
 }

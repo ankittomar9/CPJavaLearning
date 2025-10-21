3120d4398ecce09c97aa0240aca0f46f7c06d907
tomar <tomarankit78@gmail.com>
2025-10-21 23:32:08 +0530

    ArrayList Programs

--- ArrayList_Unique2.java
+++ ArrayList_Unique2.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 public class ArrayList_Unique2 {
     public static void main(String args[]){
-        ArrayList<Integer> list=new ArrayList<>(2,4,6,8,1,2,4,6,7,1);
+        ArrayList<Integer> list=new ArrayList<>((2,4,6,8,1,2,4,6,7,1));
         Collections.sort(list);
         System.out.println(list);
         long total_steps=0;

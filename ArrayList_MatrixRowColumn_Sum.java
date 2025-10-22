fe09b83433d6a595218cdeb28df935a571f4075e
tomar <tomarankit78@gmail.com>
2025-10-22 22:05:59 +0530

    ArrayList Programs

---
+++ ArrayList_MatrixRowColumn_Sum.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_MatrixRowColumn_Sum {
+    public static void main(String args[]){
+        //Scanner scn=new Scanner(System.in);
+
+    List<List<Integer>>list=new ArrayList<>(
+        List.of(
+            List.of(1,2,3),
+            List.of(4,5,6,7),
+            List.of(8)
+        )
+    );    
+
+    System.out.println("2D ArrayList Initialized:");
+        System.out.println(list); 
+        // Output: [[1, 2, 3], [4, 5, 6, 7], [8]]
+
+        // Accessing an element (e.g., the '5' in the second row, index 1)
+        int element = list.get(1).get(1);
+        System.out.println("Element at [1][1]: " + element); // Output: 5
+    }
+
+
+}

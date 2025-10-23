9d2543f21b3152126fc2026eb0c3ccded4d7e311
tomar <tomarankit78@gmail.com>
2025-10-23 23:32:04 +0530

    ArrayList Programs

--- ArrayList_Unique.java
+++ ArrayList_Unique.java
@@ -41,4 +41,6 @@ public class ArrayList_Unique {
 
 /*You are given an array A of N elements. You have to make all elements unique.
 To do so, in one step you can increase any number by one.
-Find the minimum number of steps. */
+Find the minimum number of steps. 
+Questions is done by greedy approach
+*/

17ecc35cd887e38d4fb5b532f02d19dc82a169cc
tomar <tomarankit78@gmail.com>
2025-10-24 16:16:58 +0530

    ArrayList Programs

--- ProductOf_elements.java
+++ ProductOf_elements.java
@@ -3,8 +3,16 @@ import java.util.List;
 
 public class ProductOf_elements {
     public static void main(String args[]){
-        List<Integer> list=new ArrayList<>(List.of(1,2,7,6,3,4,6,7,1,8,2,4));
-        System.out.println(list);
+        List<Integer> list2=new ArrayList<>(List.of(1,2,7,6,3,4,6,7,1,8,2,4));
+         List<Integer> list1=new ArrayList<>(List.of(3,4,2));
+        System.out.println(list1);
+
+        double product=1;
+        for(int i=0;i<list1.size();i++){
+            product=product*(list1.get(i));
+        }
+
+  System.out.println(product);
     
     
     }

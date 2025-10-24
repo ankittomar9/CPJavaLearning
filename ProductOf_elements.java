1637b1c52e6f775e64239e63b92d7fde3e265590
tomar <tomarankit78@gmail.com>
2025-10-24 16:14:22 +0530

    ArrayList Programs

---
+++ ProductOf_elements.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductOf_elements {
+    public static void main(String args[]){
+        List<Integer> list=new ArrayList<>(List.of(1,2,7,6,3,4,6,7,1,8,2,4));
+        System.out.println(list);
+    
+    
+    }
+
+    
+}

4b9962fafcab2d8d5867cbbff19705660769025d
tomar <tomarankit78@gmail.com>
2025-10-27 12:48:55 +0530

    ArrayList Programs

--- Arrays_Prefix_sum.java
+++ Arrays_Prefix_sum.java
@@ -1,8 +1,15 @@
 public class Arrays_Prefix_sum {
     public static void main(String args[]){
         int arr[]={2,4,6,8,10};
-        int prefix_arr=new prefix_arr[arr.length];
+        int prefix_arr[]=new  prefix_arr[arr.length];
 
+        prefix_arr[0]=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            prefix_arr[i]=prefix_arr[i-1]+arr[i];
+        }
+
+        System.out.println(Arrays.toString(prefix_arr));
     }
     
 }

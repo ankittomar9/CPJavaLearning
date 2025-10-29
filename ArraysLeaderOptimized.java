40e5245823ee8e7b286812e1a0f5a770ed07f061
tomar <tomarankit78@gmail.com>
2025-10-29 11:21:20 +0530

    programs updated

---
+++ ArraysLeaderOptimized.java
@@ -0,0 +1,13 @@
+public class ArraysLeaderOptimized {
+    
+}
+
+
+
+/*Leaders in a Array
+ * 
+ * Whats a  Leader:
+ * An element that is strictly greater
+ *  than all the elements on its right side
+ * A[i]> A>[i+1 ... n-1]
+ */

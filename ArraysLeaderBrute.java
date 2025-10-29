661c0dd0960733b66accbd3517f7ff0a0532e96d
tomar <tomarankit78@gmail.com>
2025-10-29 11:21:43 +0530

    Arrays Programs[Learning]

--- ArraysLeaderBrute.java
+++ ArraysLeaderBrute.java
@@ -1,4 +1,13 @@
 public class ArraysLeaderBrute {
-    
+
     
 }
+
+
+/*Leaders in a Array
+ * 
+ * Whats a  Leader:
+ * An element that is strictly greater
+ *  than all the elements on its right side
+ * A[i]> A>[i+1 ... n-1]
+ */

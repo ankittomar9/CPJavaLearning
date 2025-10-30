29b321eaa3984625a1a85ef023c863121d82ebf2
tomar <tomarankit78@gmail.com>
2025-10-31 01:34:55 +0530

    Arrays Programs[Learning]

--- ArraysLeaderBrute.java
+++ ArraysLeaderBrute.java
@@ -1,6 +1,27 @@
 public class ArraysLeaderBrute {
+ 
+    public static void main(String args[]){
+       int arr[]={10,7,9,4,3,2}; 
+       int ans=0;
+       for(int i=0;i<arr.length;i++){
+          boolean flag=true;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]>=arr[i]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag){
+            ans++;
+        }
 
+
+       }
+       System.out.println(ans);
+
+    }
     
+
 }
 
 /*Leaders in a Array

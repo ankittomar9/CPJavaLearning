0e26316ddcda529b197dd1cd5cbf2c27aa3b5f5d
tomar <tomarankit78@gmail.com>
2025-10-30 18:01:29 +0530

    Arrays Programs[Learning]

--- ArraysAGpairOptimized.java
+++ ArraysAGpairOptimized.java
@@ -1,3 +1,24 @@
 public class ArraysAGpairOptimized {
+    public static void main(String args[]){
+
+      String s="bcaggaag"; 
+      int n=s.length();
+        int count=0;int ans=0;
+
+        for(int i=n-1;i>=0;i++){
+            
+        }
+
+
+    }
     
 }
+
+
+
+/*
+ * For every 'a' -> looking for count of g on right hand side
+ * for every 'g'-> looking for count of a on left hand side
+ * 
+ * 
+ */

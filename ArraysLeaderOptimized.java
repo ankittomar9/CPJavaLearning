eb2cf1a12311ebd2c32563877781250c4bc42217
tomar <tomarankit78@gmail.com>
2025-11-01 09:58:28 +0530

    Arrays Programs[Learning]

--- ArraysLeaderOptimized.java
+++ ArraysLeaderOptimized.java
@@ -3,10 +3,14 @@ public class ArraysLeaderOptimized {
     public static void main(String args[]){
         int[] arr={10,7,9,3,2,4};
         int n=arr.length;
-       int max=n-1;
-        int ans=0;
-
-        for(int n-2;i>=0;i++ ){
+       int max=arr[n-1];
+        int ans=1;
+        int i;
+        for( i=n-2; i>=0;i--){
+            if(arr[i]>max){
+                ans++;
+                max=arr[i];
+            }
 
         }
 
@@ -16,7 +20,8 @@ public class ArraysLeaderOptimized {
     
 }
 
-/*Leaders in a Array
+/* Carry Forward approach
+Leaders in a Array
  * 
  * Whats a  Leader:
  * An element that is strictly greater

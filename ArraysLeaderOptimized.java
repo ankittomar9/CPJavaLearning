35df3c0a47dc132c7f56fae52187ddb612b0c4d6
tomar <tomarankit78@gmail.com>
2025-11-01 09:51:12 +0530

    Arrays Programs[Learning]

--- ArraysLeaderOptimized.java
+++ ArraysLeaderOptimized.java
@@ -1,4 +1,18 @@
 public class ArraysLeaderOptimized {
+
+    public static void main(String args[]){
+        int[] arr={10,7,9,3,2,4};
+        int n=arr.length;
+       int max=n-1;
+        int ans=0;
+
+        for(int n-2;i>=0;i++ ){
+
+        }
+
+
+        System.out.println(ans);
+    }
     
 }
 

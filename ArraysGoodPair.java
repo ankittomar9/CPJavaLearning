0ff61d5cd0cf603e56bf3252c0104384b6c19011
tomar <tomarankit78@gmail.com>
2025-11-05 21:40:48 +0530

    Arrays Programs[Learning]

--- ArraysGoodPair.java
+++ ArraysGoodPair.java
@@ -1,13 +1,21 @@
+
+import java.util.Scanner;
+
 public class ArraysGoodPair {
     public static void main(String args[]){
         int arr[]={2,4,6,9,7,1,5,3,6,4,2};
         Scanner scn=new Scanner(System.in);
 
-        int x=scn.nextInt();
-
+        int k=scn.nextInt();
+        int count=0;
         for(int i=0;i<arr.length;i++){
-            
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==k){
+                    count++;
+                }
+            }
         }
+        System.out.println(count);
 
     }
 }

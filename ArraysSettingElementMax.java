10468e04a2c0d2bb811a9d8a2be800c0eeb43f10
tomar <tomarankit78@gmail.com>
2025-11-06 17:56:51 +0530

    Arrays Programs[Learning]

---
+++ ArraysSettingElementMax.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ArraysSettingElementMax {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int arr[]={2,4,3,1,5};
+        int n=scn.nextInt();
+
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}

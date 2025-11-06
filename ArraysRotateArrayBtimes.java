3415b9f69e58979c1c7c8daed40c60a6791deb98
tomar <tomarankit78@gmail.com>
2025-11-06 11:13:47 +0530

    Arrays Programs[Learning]

---
+++ ArraysRotateArrayBtimes.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArraysRotateArrayBtimes {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+       int arr[]={1,2,3,4};
+        int n=scn.nextInt(); // 2
+        
+            RotateArray(arr,n);
+            System.out.println(Arrays.toString(arr));
+
+            }
+    
+            public static int[] RotateArray(int arr[],int n){
+
+            int i=0; int j=arr.length-1;
+            while(i<j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;j--;
+            }       
+            return arr;
+
+            }
+
+}
+
+
+/*
+ * 
+ * 
+ * 
+ */

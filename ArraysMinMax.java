5838328a784f99c45fb128377924a28857c8a536
tomar <tomarankit78@gmail.com>
2025-11-21 12:18:31 +0530

    Java Programs Learning

---
+++ ArraysMinMax.java
@@ -0,0 +1,26 @@
+public class ArraysMinMax {
+    public static void main(String args[]){
+       int arr[]={1,2,3,4,5}; int a=0;int b=0;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+       for(int i=0;i<arr.length;i++){
+         if(arr[i]>max){
+            max=arr[i];
+            a=i;
+        }
+         if(arr[i]<min){
+            min=arr[i];
+            b=i;
+        }
+
+       }
+            System.out.println("Found Max  at " +a+ " Value : "+arr[a]);
+     
+        
+             System.out.println("Found Min  at " +b+ " Value : "+arr[b]);
+        }
+
+}
+    
+

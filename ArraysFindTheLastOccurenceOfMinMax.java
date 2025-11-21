c5aebbb200feb5f0b46e117bd88c1663ff5a01a1
tomar <tomarankit78@gmail.com>
2025-11-21 12:24:57 +0530

    Java Programs Learning

--- ArraysFindTheLastOccurenceOfMinMax.java
+++ ArraysFindTheLastOccurenceOfMinMax.java
@@ -1,15 +1,15 @@
-public class ArraysMinMax {
+public class ArraysFindTheLastOccurenceOfMinMax {
     public static void main(String args[]){
-       int arr[]={1,2,3,4,5,4,10,9,16,3,4,1}; int a=0;int b=0;
+       int arr[]={1,2,3,16,5,4,16,9,16,3,4,1,7,1,3,16,10,4,5}; int a=0;int b=0;
         int min=Integer.MAX_VALUE;
         int max=Integer.MIN_VALUE;
 
        for(int i=0;i<arr.length;i++){
-         if(arr[i]>max){
+         if(arr[i]>=max){   // Occurence changes with this condition first and last
             max=arr[i];
             a=i;
         }
-         if(arr[i]<min){
+         if(arr[i]<=min){
             min=arr[i];
             b=i;
         }
@@ -20,7 +20,5 @@ public class ArraysMinMax {
         
              System.out.println("Found Min  at " +b+ " Value : "+arr[b]);
         }
-
-}
     
-
+}

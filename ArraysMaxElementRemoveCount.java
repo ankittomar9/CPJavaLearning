f919ecc37de467f03191b6209a010069117be73a
tomar <tomarankit78@gmail.com>
2025-11-21 20:23:52 +0530

    Java Programs Learning

--- ArraysMaxElementRemoveCount.java
+++ ArraysMaxElementRemoveCount.java
@@ -1,17 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArraysMaxElementRemoveCount {
     public static void main(String args[]){
         int arr[]={2,4,3,1,5}; int b=3;
         int NoOfOperationsCount=0;
-
+        List<Integer> list = new ArrayList<>();
         for(int i=0;i<arr.length;i++){
             if(arr[i]>=b){
                 NoOfOperationsCount++;
+               list.add(i);
             }
         }
         
         System.out.println("Number of operations required : "+
         NoOfOperationsCount);
-
+        System.out.println("Locations needed to update : "+list);
 
     }
     

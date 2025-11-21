e3e6fe22029d5446afae7b6807e8ff4be6986377
tomar <tomarankit78@gmail.com>
2025-11-22 00:32:36 +0530

    Java Programs Learning

--- Arrays_Minimum_Picks.java
+++ Arrays_Minimum_Picks.java
@@ -1,19 +1,26 @@
 public class Arrays_Minimum_Picks {
     public static void main(String args[]){
         int arr[]={5,17,100,1};
-        int max_even=0;        int max_odd=0;
+        int max_even=Integer.MIN_VALUE;        int min_odd=Integer.MAX_VALUE;
 
         for(int i=0;i<arr.length;i++){
             if(arr[i]%2==0){
-                max_even=arr[i];
+                max_even = Math.max(max_even, arr[i]);
+             
             }
 
             if(arr[i]%2!=0){
-                max_odd=arr[i];
+                   min_odd=Math.min(min_odd, arr[i]);
+            
+              
             }
         }
-        System.out.println(max_odd);
+        int result=max_even-min_odd;
+        System.out.println(min_odd);
         System.out.println(max_even);
+        System.out.println(result);
+
+        
     }
     
 }

352fa548107c4d84c59ddc56e3cbc80740737429
tomar <tomarankit78@gmail.com>
2025-11-27 00:06:24 +0530

    Java Programs Learning

--- Arrays2DMartrixTranspose.java
+++ Arrays2DMartrixTranspose.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class Arrays2DMartrixTranspose {
     public static void main(String args[]){
-        int arr[][]={   {1,2,3},
-                        {4,5,6},
-                        {7,8,9}};
-   
-
+        int arr[][] = { 
+                        {1, 2, 3, 4},   // Row 0
+                        {5, 6, 7, 8},   // Row 1
+                        {9, 10, 11, 12} // Row 2
+                    };
     int n=arr.length;
     int m=arr[0].length;
-        int resultant[][]=new int[n][m];
+        int resultant[][]=new int[m][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 resultant[j][i]=arr[i][j];

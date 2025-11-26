e1898e5264f3fec0489e83126751371b7a14606d
tomar <tomarankit78@gmail.com>
2025-11-26 23:44:57 +0530

    Java Programs Learning

--- Arrays2DSquarePrintDiagonal.java
+++ Arrays2DSquarePrintDiagonal.java
@@ -4,7 +4,7 @@ public class Arrays2DSquarePrintDiagonal {
     public static void main(String args[]){
         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
         int n=arr.length;
-   
+   //Why Because i==j so (1,1)(2,2)(3,3)
 
         for(int i=0;i<n;i++){
             System.out.println(arr[i][i]);

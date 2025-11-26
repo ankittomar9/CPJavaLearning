26d2945b4c568552b4976cf3f5666ef9d788b46d
tomar <tomarankit78@gmail.com>
2025-11-26 23:53:00 +0530

    Java Programs Learning

--- Arrays2DAntiDiagonalPrint.java
+++ Arrays2DAntiDiagonalPrint.java
@@ -4,12 +4,20 @@ public class Arrays2DAntiDiagonalPrint {
         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
         int n=arr.length;
         int i=0;int  j=n-1;
-
+            AntiDiagonalPrinter(arr,0,n-1);
 
     }
 
-    public static void AntiDiagonalPrinter(int arr,int i,int j){
-        for(int i=)
+    public static void AntiDiagonalPrinter(int[][] arr,int i,int j){
+       int n=arr.length;
+        i=0;j=n-1; 
+
+       while(i<n && j>=0){
+        System.out.println(arr[i][j]);
+        
+        i++;
+        j--;
+       }
 
     }
     

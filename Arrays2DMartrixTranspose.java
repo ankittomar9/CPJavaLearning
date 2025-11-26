1a5fac40b5d27704a8e4e096bb4d6da866ca6e34
tomar <tomarankit78@gmail.com>
2025-11-26 23:36:02 +0530

    Java Programs Learning

---
+++ Arrays2DMartrixTranspose.java
@@ -0,0 +1,16 @@
+public class Arrays2DMartrixTranspose {
+    public static void main(String args[]){
+        int arr[][]={   {1,2,3,4},
+                        {4,5,6,11},
+                        {7,8,9,10}};
+   
+
+    int n=arr.length;
+    int m=arr[0].length;
+
+
+
+
+    
+     }
+}

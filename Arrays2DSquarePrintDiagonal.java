d429353413ff1dc8455861de312963923d2c24a7
tomar <tomarankit78@gmail.com>
2025-11-26 23:44:33 +0530

    Java Programs Learning

--- Arrays2DSquarePrintDiagonal.java
+++ Arrays2DSquarePrintDiagonal.java
@@ -1,3 +1,16 @@
+
+
 public class Arrays2DSquarePrintDiagonal {
+    public static void main(String args[]){
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int n=arr.length;
+   
+
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i][i]);
+
+        }
+
+    }  
     
 }

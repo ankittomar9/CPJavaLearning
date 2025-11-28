d37861de42585a1d813386c7f1487acdc581357f
tomar <tomarankit78@gmail.com>
2025-11-28 18:16:17 +0530

    Java Programs Learning

---
+++ Arrays2DColumWiseSum1.java
@@ -0,0 +1,24 @@
+public class Arrays2DColumWiseSum1 {
+    public static void main(String args[]){
+        int arr[][]={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}};
+        ColumWiseSumFunc(arr);
+    }
+
+    public static void ColumWiseSumFunc(int arr[][]){
+        int n=arr.length;
+        int m=arr[0].length;
+   
+        for(int j=0;j<m;j++){
+                 int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i][j];
+        }
+        System.out.println("Sum of Column "+j +" : " +sum);
+    }
+
+    }
+
+}

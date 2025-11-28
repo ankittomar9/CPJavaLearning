a56c28cd0f0c292585f9c59c6c168d6dd2d08d8c
tomar <tomarankit78@gmail.com>
2025-11-28 19:42:05 +0530

    Java Programs Learning

---
+++ Arrays2DMatrixRotationBy90degree.java
@@ -0,0 +1,7 @@
+public class Arrays2DMatrixRotationBy90degree {
+    public static void main(String args[]){
+        
+
+    }
+    
+}
---
+++ Arrays2DRectangularMatrixTranspose1.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class Arrays2DRectangularMatrixTranspose1 {
+    public static void main(String args[]){
+            int arr[][]={{1,2,3},
+                        {4,5,6},
+                        {7,8,9},
+                        {10,11,12}};
+             Arrays2DRectangularMatrixTransposeFunc(arr);
+    }
+
+    public static void Arrays2DRectangularMatrixTransposeFunc(int arr[][]){
+        int n=arr.length;
+        int m=arr[0].length;
+        if(n==0 || m==0){
+            System.out.println("Result: []");
+            return;
+        }
+
+        int resultant[][]=new int [m][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                resultant[j][i]=arr[i][j];
+            }
+        }
+        System.out.println(Arrays.deepToString(resultant));
+
+    }
+    
+}

1a72bc922daa03e633ce18ec90344ee5443e9ef7
tomar <tomarankit78@gmail.com>
2025-11-28 20:14:28 +0530

    Java Programs Learning

--- Arrays2DMatrixRotationBy90degree.java
+++ Arrays2DMatrixRotationBy90degree.java
@@ -1,7 +1,14 @@
 public class Arrays2DMatrixRotationBy90degree {
     public static void main(String args[]){
-        
+            int arr[][]={{1,2,3},
+                         {4,5,6},
+                         {7,8,9}};
+          /*Desired output
+           [    [7, 4, 1],
+                [8, 5, 2],
+                [9, 6, 3]  ]       */    }
+        public static void Arrays2DMatrixRotationBy90degreeFunc(int arr[][]){
+                  
 
     }
-    
 }
---
+++ Arrays2DMatrixScalarProduct1.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class Arrays2DMatrixScalarProduct1 {
+    public static void main(String args[]){
+        int arr[][]={
+                     {1,2,3,},
+                    {4,5,6},
+                    {7,8,9}};
+            int B=2;
+            Arrays2DMatrixScalarProductFunc(arr,B);
+
+    }
+
+    public static void  Arrays2DMatrixScalarProductFunc(int arr[][],int B){
+        int n=arr.length;
+        int m=arr[0].length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              arr[i][j]=arr[i][j]*B;
+            }
+        }
+         System.out.println(Arrays.deepToString(arr));     
+
+        
+    }
+
+
+
+    
+}

720775cdf69ca70153986af9a526d0d4672ae5e7
tomar <tomarankit78@gmail.com>
2025-11-28 12:22:22 +0530

    Java Programs Learning

--- Arrays2DRowWiseSum1.java
+++ Arrays2DRowWiseSum1.java
@@ -1,13 +1,24 @@
 public class Arrays2DRowWiseSum1 {
     public static void main(String args[]){
-        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int arr[][]={{1,2,3},
+                    {4,5,6},
+                    {7,8,9}};
 
+       RowWiseSumCalculatorFunc(arr); 
 
     }
 
     public static void RowWiseSumCalculatorFunc(int arr[][]){
+         int n=arr.length;
+         int m=arr[0].length;
 
-        
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum=sum+arr[i][j];
+            }
+            System.out.println( "Sum of row "+i+" : "+sum);
+        }
     }
     
 }

c6368f28d459571cf475269b55eaa38ec732fe85
tomar <tomarankit78@gmail.com>
2025-11-28 20:21:35 +0530

    Java Programs Learning

--- Arrays2DMatrixScalarProduct1.java
+++ Arrays2DMatrixScalarProduct1.java
@@ -1,17 +1,13 @@
-
 import java.util.Arrays;
-
 public class Arrays2DMatrixScalarProduct1 {
     public static void main(String args[]){
         int arr[][]={
                      {1,2,3,},
-                    {4,5,6},
-                    {7,8,9}};
+                     {4,5,6},
+                     {7,8,9}};
             int B=2;
             Arrays2DMatrixScalarProductFunc(arr,B);
-
     }
-
     public static void  Arrays2DMatrixScalarProductFunc(int arr[][],int B){
         int n=arr.length;
         int m=arr[0].length;
@@ -22,11 +18,7 @@ public class Arrays2DMatrixScalarProduct1 {
             }
         }
          System.out.println(Arrays.deepToString(arr));     
-
         
     }
 
-
-
-    
 }

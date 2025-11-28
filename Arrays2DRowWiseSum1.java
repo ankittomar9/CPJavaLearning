99cbcbd8cb94df09777d79318fd5c155965031df
tomar <tomarankit78@gmail.com>
2025-11-28 12:11:29 +0530

    Java Programs Learning

--- Arrays2DRowWiseSum1.java
+++ Arrays2DRowWiseSum1.java
@@ -1,3 +1,13 @@
 public class Arrays2DRowWiseSum1 {
+    public static void main(String args[]){
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+
+
+    }
+
+    public static void RowWiseSumCalculatorFunc(int arr[][]){
+
+        
+    }
     
 }

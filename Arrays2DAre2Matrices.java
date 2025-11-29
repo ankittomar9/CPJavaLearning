5dc39315d55bc97d9876b02ee61193a462429629
tomar <tomarankit78@gmail.com>
2025-11-29 19:51:55 +0530

    Java Programs Learning

--- Arrays2DAre2Matrices.java
+++ Arrays2DAre2Matrices.java
@@ -1,7 +1,21 @@
 public class Arrays2DAre2Matrices {
     public static void main(String args[]){
 
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+         int barr1[][]={{1,2,3},{4,5,6},{7,8,9}};
+          int carr2[][]={{1,2,3},{1,2,3},{1,2,3}};
+        
+
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(arr[i][j]==barr1[i][j]){
+
+                }
+            }
+          }
+
+
+
     }
-    
-    
+  
 }

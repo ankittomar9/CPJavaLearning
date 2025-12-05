7e2b6c651226d1f3e6ee8af1f533b338e88f887e
tomar <tomarankit78@gmail.com>
2025-12-05 16:12:49 +0530

    Java Programs Learning

---
+++ Arrays_Subarray_max_Sum_Brute.java
@@ -0,0 +1,31 @@
+public class Arrays_Subarray_max_Sum_Brute {
+    public static void main(String args[]){
+        int nums[]={-3,4,-2,5,3,-2,8,2,-1,4};
+        int k=5;
+        SubarrayCalculator(nums,k);
+
+    }
+
+    public static void SubarrayCalculator(int nums[],int k){
+        int n=nums.length; int start=0;
+        int end=k-1; int ans=Integer.MIN_VALUE;
+            while(end<n){
+                int sum=0;
+            for(int i=start;i<=end;i++){
+                sum=sum+nums[i];
+                }
+                
+       System.out.println("Subarray Sum from index "+start+" to "+end+" : " + sum);
+           if(sum>ans){
+                    ans=sum;
+                }
+          start++;end++;
+
+        }
+        System.out.println("----------------------------------------------");
+        System.out.println("The Overall Maximum Subarray Sum is: " + ans);
+
+    }
+}
+    
+

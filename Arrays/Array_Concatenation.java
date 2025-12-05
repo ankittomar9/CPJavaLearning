b8bcd6c6041182809aa8216242a76df07bb62401
tomar <tomarankit78@gmail.com>
2025-12-05 11:34:33 +0530

    Java Programs Learning

---
+++ Arrays/Array_Concatenation.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+
+
+public class Array_concatenation {
+    public static void main(String args[]){
+        int[] arr={1,2,3};
+        //Concat the array  
+         int n=arr.length;
+        if(n==0){
+            System.out.println("Array is Empty : ");
+        }
+       
+        int arr2[]=new int[2*n];
+
+        for(int i=0;i<n;i++){   
+            arr2[i]=arr[i];  
+            arr2[i+n]=arr[i]; 
+        }
+
+        System.out.println(Arrays.toString(arr));
+         System.out.println(Arrays.toString(arr2));
+
+    }
+    
+}

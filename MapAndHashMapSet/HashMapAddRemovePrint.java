748904d8777027e3b2d678815d33d086074891bd
tomar <tomarankit78@gmail.com>
2025-12-09 12:12:44 +0530

    Java Programs Learning

--- MapAndHashMapSet/HashMapAddRemovePrint.java
+++ MapAndHashMapSet/HashMapAddRemovePrint.java
@@ -1,5 +1,22 @@
 package MapAndHashMapSet;
 
+import java.util.HashMap;
+
 public class HashMapAddRemovePrint {
+    public static void main(String args[]){
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(1,10);
+        hm.put(2,20);
+        hm.put(3,30); // removed
+        hm.put(4,40);
+        hm.put(5,50);
+
+        hm.remove(3);
+       // hm.remove(hm.values(50)); Not Allowed
+
+        System.out.println("Removed a key value pair \n"+hm);
+        
+
+    }
     
 }

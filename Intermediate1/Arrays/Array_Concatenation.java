38a7f0cf4d014c0ed214ecc67be545ec3f264de3
tomar <tomarankit78@gmail.com>
2025-12-09 16:45:05 +0530

    Java Programs Learning

--- Intermediate1/Arrays/Array_Concatenation.java
+++ Intermediate1/Arrays/Array_Concatenation.java
@@ -1,8 +1,6 @@
-
+package Arrays;
 import java.util.Arrays;
-
-
-public class Array_concatenation {
+public class Array_Concatenation {
     public static void main(String args[]){
         int[] arr={1,2,3};
         //Concat the array  

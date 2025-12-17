82536f9893cc7b007eb271113c12eb408e610651
tomar <tomarankit78@gmail.com>
2025-12-18 01:58:40 +0530

    New Learning Java Programs Added

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -2,8 +2,28 @@ package BinarySearch;
 
 public class BinarySearch {
     public static void main(String args[]){
-        
+       int[] nums={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int k=8;
+        BinarySearchFunc(nums,k);    }
 
-    }
-    
+    public static void BinarySearchFunc(int nums[],int target){
+        int n=nums.length; int low=0;int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+           if(nums[mid]==target){
+            System.out.println("tagret found at  : "+mid);
+                return;
+           }
+
+           else if(nums[mid]<target){
+           low=mid+1;
+           }
+           else{
+            high=mid-1;
+           }
+
+        }
+        System.out.println("Target Not Found" );
+    }    
 }

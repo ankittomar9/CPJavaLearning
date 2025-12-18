921af1a0db6ceec9847c3b9d3264109f4f538292
tomar <tomarankit78@gmail.com>
2025-12-18 22:53:34 +0530

    New Learning Java Programs Added

--- BinarySearch/BinarySearchSqrt.java
+++ BinarySearch/BinarySearchSqrt.java
@@ -1,5 +1,39 @@
 package BinarySearch;
 
 public class BinarySearchSqrt {
+    public static void main(String args[]){
+        long x=32; // worked for 2 4 8 16 not for 32 but worked for 64
+        if(x<0){
+            System.out.println("Negative Number doesn't allowed " );
+        }
+        if(x==0 || x==1){
+            System.out.println("Square root of x is : " + x);
+            return;
+        }
+
+
+        long low=1; long high=x;
+        long ans=1;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            long val=mid*mid;
+            
+            if(val==x){
+                System.out.println("Square root of x is : "+mid);
+               return;
+            }
+
+            else if(val<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }    
+
+        }
+        System.out.println("Square root of x is : " + ans);
+
+    }
     
 }

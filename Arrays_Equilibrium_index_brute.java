d35af64fb82c6da92ebfa74c4370be3c0a593976
tomar <tomarankit78@gmail.com>
2025-12-19 23:56:13 +0530

    New Learning Java Programs Added

---
+++ Arrays_Equilibrium_index_brute.java
@@ -0,0 +1,37 @@
+public class Arrays_Equilibrium_index_brute {
+    public static void main(String args[]){
+      int arr[]={-7, 1, 5, 2, -4, 3, 0};
+      int n=arr.length;
+
+        //select all  indexes from 0 to n-1
+        for(int i=0;i<arr.length;i++){
+            
+            //Calculate sum before i that is 0 to i-1
+
+            int left_Sum=0;
+            for(int j=0;j<=i-1;j++){
+                left_Sum=left_Sum+arr[j];
+            }
+
+            int right_Sum=0;
+            for(int j=i+1;j<=n-1;j++){
+                right_Sum=right_Sum+arr[j];
+            }    
+
+            if(left_Sum==right_Sum){
+                System.out.println("Equlibrium index found at index "+i );
+                  System.out.println("Left Sum : "+left_Sum+ " == Right Sum "+right_Sum );
+                  return;
+
+            }
+        }
+          System.out.println("Equlibrium index doesn't exists");   
+
+
+
+
+
+
+    }
+    
+}
--- Arrays_Equlibrium_index_Optimized.java
+++ Arrays_Equlibrium_index_Optimized.java
@@ -1,32 +1,43 @@
 import java.util.Arrays;
-
 public class Arrays_Equlibrium_index_Optimized {
     public static void main(String args[]){
-         int arr[]={1,2,3,4,8,10}; int n=arr.length;
-         int pf_arr[]=new int[n];
-         pf_arr[0]=arr[0];
+          int arr[]={-7, 1, 5, 2, -4, 3, 0};
+          int n=arr.length;
+         int prefix_arr[]=new int[n];
 
-         for(int i=1;i<n;i++){
-            pf_arr[i]=pf_arr[i-1]+arr[i];
-         }
-         System.out.println("Prefix Sum Array: " + Arrays.toString(pf_arr));
+         prefix_arr[0]=arr[0];
+         for(int i=1;i<arr.length;i++){
+            prefix_arr[i]=prefix_arr[i-1]+arr[i];
+         }    
+        System.out.println("original array : ");
+         System.out.println(Arrays.toString(arr));
+         System.out.println("Prefix array : ");
+         System.out.println(Arrays.toString(prefix_arr));
 
-         int equilibrium_index=-1; int value_at_equilibrium_index=-1;
          for(int i=0;i<n;i++){
-            if(pf_arr[i-1]==pf_arr[n-1]-pf_arr[i]){
-                equilibrium_index=i;
-                value_at_equilibrium_index=arr[i];
 
-              
+            int total_sum=prefix_arr[n-1];
+            //left sum
+            int left_sum=0;
+            if(i==0){
+                left_sum=0;
+            }else{
+                left_sum=prefix_arr[i-1];
             }
+            //right sum 
+            int right_sum=0;
+             right_sum=total_sum-prefix_arr[i];
+
+            
+            if(left_sum==right_sum){
+                System.out.println("Equlibrium index found at index "+i );
+                  System.out.println("Left Sum : "+left_sum+ " == Right Sum "+right_sum );
+                  return;
+
+            }
+             
          }
-
-           System.out.println("equilibrium_index : "+equilibrium_index);
-           System.out.println("value_at_equilibrium_index : "+value_at_equilibrium_index);
-
-
-
-         
+          System.out.println("Equlibrium index doesn't exists");  
 
     }
     

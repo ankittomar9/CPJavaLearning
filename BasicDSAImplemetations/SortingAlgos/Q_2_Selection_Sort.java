3a40fac04e077432d34994c15a4c1259a625c3e5
tomar <tomarankit78@gmail.com>
2025-12-23 20:50:02 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/SortingAlgos/Q_2_Selection_Sort.java
+++ BasicDSAImplemetations/SortingAlgos/Q_2_Selection_Sort.java
@@ -8,18 +8,19 @@ public class Q_2_Selection_Sort {
        SelectionSortFunc(nums);
        System.out.println(" After Sort :\n " + Arrays.toString(nums));
     }
-
+    
     public static void SelectionSortFunc(int nums[]){
         int n=nums.length;
 
         for(int i=0;i<n-1;i++){
             int minIndex=i;
-
+           
             for(int j=i+1;j<n;j++){
                 if(nums[j]<nums[minIndex]){
                     minIndex=j;
                 }
             }
+
             int temp=nums[minIndex];
             nums[minIndex]=nums[i];
             nums[i]=temp;

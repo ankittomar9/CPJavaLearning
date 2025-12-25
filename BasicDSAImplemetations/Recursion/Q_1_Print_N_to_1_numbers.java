ce4ccbe664e1302485f5c7342f0ba2ebd9d31afd
tomar <tomarankit78@gmail.com>
2025-12-25 19:59:03 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/Recursion/Q_1_Print_N_to_1_numbers.java
+++ BasicDSAImplemetations/Recursion/Q_1_Print_N_to_1_numbers.java
@@ -2,8 +2,21 @@ package BasicDSAImplemetations.Recursion;
 
 public class Q_1_Print_N_to_1_numbers {
     public static void main(String args[]){
+        int n=5;
+        System.out.println("--- Printing N to 1 ---");
+        printNto1(n);
+        
 
     }
-    
+
+    public static void printNto1(int n){
+        if(n==0) // base case    
+            return ;
+
+        System.out.println(n+" ");
+        printNto1(n-1);
+
+    }
+
     
 }

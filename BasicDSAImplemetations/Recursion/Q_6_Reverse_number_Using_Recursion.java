f2e251efb42d2cb5fca4716e08044d20fae28889
tomar <tomarankit78@gmail.com>
2025-12-26 00:12:12 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/Recursion/Q_6_Reverse_number_Using_Recursion.java
+++ BasicDSAImplemetations/Recursion/Q_6_Reverse_number_Using_Recursion.java
@@ -1,5 +1,23 @@
 package BasicDSAImplemetations.Recursion;
 
 public class Q_6_Reverse_number_Using_Recursion {
+    public static void main(String args[]){
+        int n=1234;
+        //Expected output=4321
+
+        //System.out.println("Reverse Number is " );
+               
+
+    }
+
+    public static void ReverseHelper(int n){
+        if(n==0){
+            return;
+        }
+       
+       System.out.println(n+" ");
+        n=n%10;
+        ReverseHelper(n/10);
+    }
     
 }

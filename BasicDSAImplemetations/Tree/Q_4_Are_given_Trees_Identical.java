b8a6ca59b5cdb5b17dd3781008c02c65dc1dd036
tomar <tomarankit78@gmail.com>
2025-12-30 15:41:58 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Tree/Q_4_Are_given_Trees_Identical.java
@@ -0,0 +1,61 @@
+package BasicDSAImplemetations.Tree;
+
+public class Q_4_Are_given_Trees_Identical {
+    public static void main(String args[]){
+         // Build the Tree: 50(root), 30(L), 70(R), 20(LL), 40(LR)
+         TreeNode A=new TreeNode(50);
+         A.left=new TreeNode(30);
+         A.right=new TreeNode(70);
+         A.left.left=new TreeNode(20);
+         A.left.right=new TreeNode(40);
+
+            // Build the Tree Similar : 50(root), 30(L), 70(R), 20(LL), 40(LR)
+         TreeNode B=new TreeNode(50);
+         B.left=new TreeNode(30);
+         B.right=new TreeNode(70);
+         B.left.left=new TreeNode(20);
+         B.left.right=new TreeNode(40);
+
+         boolean isCheck=isTreeIdentical(A,B);
+         if(isCheck==true){
+            System.out.println("Trees are Identical");
+         }else{
+            System.out.println("Tress are Not identical");
+         }
+    }
+    public static class TreeNode{
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+
+
+
+    }
+
+
+    public static boolean isTreeIdentical(TreeNode A,TreeNode B){
+        if(A ==null && B==null)
+            return true;
+
+        if(A ==null || B==null)
+            return false;
+
+        if(A.val!=B.val){
+            return false;
+        }
+
+        boolean leftSame=isTreeIdentical(A.left,B.left);
+          boolean rightSame=isTreeIdentical(A.right,B.right);
+
+        return leftSame && rightSame;
+    }
+
+
+}
---
+++ BasicDSAImplemetations/Tree/Q_5_Tree_Traversal_Pre_Order.java
@@ -0,0 +1,6 @@
+package BasicDSAImplemetations.Tree;
+
+public class Q_5_Tree_Traversal_Pre_Order {
+    
+    
+}

e5349c9efc8091ecbdc10350cf7c4d060d917fda
tomar <tomarankit78@gmail.com>
2025-12-31 13:03:46 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/LinkedList/Q_2_Traverse_Linked_List_using_loop.java
@@ -0,0 +1,27 @@
+package BasicDSAImplemetations.LinkedList;
+
+public class Q_2_Traverse_Linked_List_using_loop {
+    
+    public static void main(String args[]){
+            Node node=new Node(10);
+            node.next=new Node(20);
+            node.next.next=new Node(30);
+            node.next.next.next=new Node(40);
+        
+        Node temp=node;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+
+    }
+    public static  class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+}

c2440d731f13725cc52670b827677a6220cf8746
tomar <tomarankit78@gmail.com>
2026-01-01 20:31:38 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Array/Q_1_Array_print_all_elements_using_loops.java
@@ -0,0 +1,25 @@
+package BasicDSAImplemetations.Array;
+
+public class Q_1_Array_print_all_elements_using_loops {
+    public static void main(String args[]){
+        int nums[]={1,2,3,4,5,6,7,8,9};
+
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+
+        System.out.println();
+        int a=0;int b=nums.length-1;
+        while(a<=b){
+            System.out.print(nums[a]+" ");
+            a++;
+        }
+         System.out.println();
+            for(int variable: nums){
+                 System.out.print(variable +" ");
+            }
+
+        
+    }
+    
+}

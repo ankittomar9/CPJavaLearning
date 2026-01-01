6a626db6223385564519b1b13232173f25bb0130
tomar <tomarankit78@gmail.com>
2026-01-01 20:55:43 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/Array/Q_2_Array_Sum_Average_using_traditional_Loop.java
+++ BasicDSAImplemetations/Array/Q_2_Array_Sum_Average_using_traditional_Loop.java
@@ -13,12 +13,13 @@ public class Q_2_Array_Sum_Average_using_traditional_Loop {
          System.out.println("Average is \n"+average);
 
          int a=0;int b=nums.length; int newSum=0; double newAverage=0; 
-        while(a<=b){
-            sum=sum+nums[a];
+        while(a<b){    
+           //  System.out.println("Test  \n"); used this to check overflow i did a<=b
+            newSum=newSum+nums[a];
             a++;
         }
          System.out.println("newSum is \n"+newSum);
-         newAverage=(newSum/b);
+         newAverage=(double)(newSum/b);
 
         System.out.println("newAverage is \n"+newAverage);
     }

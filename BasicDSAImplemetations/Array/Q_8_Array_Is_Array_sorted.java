6203a5a415fac8c05a0656a3bf1a315a45488622
tomar <tomarankit78@gmail.com>
2026-01-01 22:46:10 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Array/Q_8_Array_Is_Array_sorted.java
@@ -0,0 +1,25 @@
+package BasicDSAImplemetations.Array;
+
+public class Q_8_Array_Is_Array_sorted {
+    public static void main(String args[]){
+      //  int nums[]={10,20,30,40,50,60};
+        int nums[]={5,10,20,40,30,60,10,50};
+        boolean isSorted=true;
+        int Inspector=0;
+        for(int i=0;i<nums.length-1;i++){   //nums.length -1 is basically last second index
+            if(nums[i]>nums[i+1]){
+                 Inspector=i;
+                isSorted=false;
+                break;
+            }
+        }
+        if(isSorted==true){
+             System.out.println("\n Array is Sorted \n");
+        }else{
+             System.out.println("\n Array is  not Sorted \n");
+              System.out.println("\n Inspector caught violation at : "+Inspector);
+        }
+
+    }
+    
+}

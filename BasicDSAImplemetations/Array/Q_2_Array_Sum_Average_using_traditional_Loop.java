773b322193f3f74115e16d56aa1c1042eeec380b
tomar <tomarankit78@gmail.com>
2026-01-01 20:31:41 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Array/Q_2_Array_Sum_Average_using_traditional_Loop.java
@@ -0,0 +1,26 @@
+package BasicDSAImplemetations.Array;
+
+public class Q_2_Array_Sum_Average_using_traditional_Loop {
+    public static void main(String args[]){
+        int nums[]={1,2,3,4,5};
+        int sum=0; double average=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        System.out.println("Sum is \n"+sum);
+        average=(double)sum/nums.length;
+
+         System.out.println("Average is \n"+average);
+
+         int a=0;int b=nums.length; int newSum=0; double newAverage=0; 
+        while(a<=b){
+            sum=sum+nums[a];
+            a++;
+        }
+         System.out.println("newSum is \n"+newSum);
+         newAverage=(newSum/b);
+
+        System.out.println("newAverage is \n"+newAverage);
+    }
+    
+}

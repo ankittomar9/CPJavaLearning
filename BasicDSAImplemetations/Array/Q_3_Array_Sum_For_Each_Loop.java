0226c9cd07f20ae1100649e0f7e9c13f88af5e35
tomar <tomarankit78@gmail.com>
2026-01-01 20:55:48 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Array/Q_3_Array_Sum_For_Each_Loop.java
@@ -0,0 +1,21 @@
+package BasicDSAImplemetations.Array;
+
+public class Q_3_Array_Sum_For_Each_Loop {
+    public static void main(String args[]){
+       int nums[]={1,2,3,4,5}; 
+        
+       int sum=0; double average;
+
+       for(int variable:nums){
+        sum=sum+variable;
+       }
+
+       System.out.println(" Sum " +sum );
+
+        average =(double)sum/nums.length;    
+
+       System.out.println(" Average : \n " +average );
+   
+    }
+    
+}

c913ca431f451b7cbc479d23af59830784098e6b
tomar <tomarankit78@gmail.com>
2026-01-01 21:24:19 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Array/Q_6_Array_Reverse_the_array.java
@@ -0,0 +1,30 @@
+package BasicDSAImplemetations.Array;
+
+import java.util.Arrays;
+
+public class Q_6_Array_Reverse_the_array {
+    public static void main(String args[]){
+        int nums[]={ 10,20,30,40,50,60,70};
+            System.out.println(Arrays.toString(nums));
+            int i=0;int j=nums.length-1;
+            while(i<j){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+
+                i++;
+                j--;
+            }
+
+            for(int variable: nums){
+                 System.out.print(variable+" ");
+            }
+
+            for(int a=0;a<nums.length;a++){
+                System.out.println(nums[a]+" ");
+            }
+           
+
+    }
+    
+}

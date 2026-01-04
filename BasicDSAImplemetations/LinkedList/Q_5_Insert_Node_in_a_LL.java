7698c2f50bc3eda528dd8a5e1798729b5d5cf431
tomar <tomarankit78@gmail.com>
2026-01-04 21:53:32 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/LinkedList/Q_5_Insert_Node_in_a_LL.java
+++ BasicDSAImplemetations/LinkedList/Q_5_Insert_Node_in_a_LL.java
@@ -1,4 +1,4 @@
-package basicDSAImplemetations.LinkedList;
+package BasicDSAImplemetations.LinkedList;
 
 public class Q_5_Insert_Node_in_a_LL {
     public static void main(String args[]){
@@ -14,7 +14,6 @@ public class Q_5_Insert_Node_in_a_LL {
         // Node node =new Node(5); this replace the Node with data 10
         // head =new Node(5);  this will change the head but he connection to next nodes is broken
         // we have head and we have head in temp 
-
         Node newNode=new Node(5);
 
             newNode.next=head;     // 5->10 but where it head its at node 10 head has 10

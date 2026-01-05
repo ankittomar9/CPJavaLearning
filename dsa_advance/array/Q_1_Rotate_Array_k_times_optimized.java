2d312a277caa9daa86c7588b97bc2dd42e784219
tomar <tomarankit78@gmail.com>
2026-01-05 23:13:22 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_1_Rotate_Array_k_times_optimized.java
@@ -0,0 +1,8 @@
+package dsa_advance.array;
+
+public class Q_1_Rotate_Array_k_times_optimized {
+    public static void main(String args){
+        int arr[]={4,1,2,3};  // expected output is : 3 1 4 2
+        int k=2; 
+    }
+}

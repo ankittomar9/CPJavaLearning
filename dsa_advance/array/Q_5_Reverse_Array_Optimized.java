7778d788af152ff28788f4707b117e3deddecab1
tomar <tomarankit78@gmail.com>
2026-01-06 21:32:44 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_5_Reverse_Array_Optimized.java
@@ -0,0 +1,26 @@
+package dsa_advance.array;
+
+import java.util.Arrays;
+
+public class Q_5_Reverse_Array_Optimized {
+public static void main(String args[]){
+    int arr[]={1,2,3,4,5,6};
+    System.out.println("Original Array: \n "+Arrays.toString(arr));
+    Array_reverse_helper(arr);
+}   
+    public static void  Array_reverse_helper(int arr[]){
+        int i=0;int j=arr.length-1;
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+
+        System.out.println("Reversed Array: \n "+Arrays.toString(arr));
+    }
+
+
+    
+}

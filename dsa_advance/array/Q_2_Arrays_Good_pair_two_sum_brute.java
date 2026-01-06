7839fa59dc3499fe431f214645b26e467f1eb070
tomar <tomarankit78@gmail.com>
2026-01-06 20:33:06 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_2_Arrays_Good_pair_two_sum_brute.java
@@ -0,0 +1,24 @@
+package dsa_advance.array;
+
+public class Q_2_Arrays_Good_pair_two_sum_brute {
+    public static void main(String args[]){
+        //Find pairs which follows this arr[i]+arr[j]==target
+        int nums[]={1,2,3,4,4,5};
+        int target=7; 
+       
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                 if(nums[i]+nums[j]==target){
+                    System.out.println("{"+nums[i]+" , " + nums[j] +"}");
+
+            }
+        }
+
+    }
+
+
+
+    }
+    
+}

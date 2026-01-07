c4a30acf68911e6427680eb3b910c9c3d44cfffc
tomar <tomarankit78@gmail.com>
2026-01-07 22:28:21 +0530

    New Learning Java Programs Added

--- dsa_advance/array/Q_8_Arrays_Find_Second_largest_element.java
+++ dsa_advance/array/Q_8_Arrays_Find_Second_largest_element.java
@@ -7,10 +7,8 @@ public class Q_8_Arrays_Find_Second_largest_element {
         // TC O(nlogn) inefficient
         //Approach 3 : maintain two variable state of Max
         int nums[]={10,2,99,3,4,55,21,25,33};
-
         int max=Integer.MIN_VALUE;
         int second_max=max;
-
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]>max){
@@ -27,8 +25,6 @@ public class Q_8_Arrays_Find_Second_largest_element {
              System.out.println("Maximum element is  : \n"+max);
         System.out.println("Second Maximum element is  : \n"+second_max);
         }
-
-       
     }
     
 }

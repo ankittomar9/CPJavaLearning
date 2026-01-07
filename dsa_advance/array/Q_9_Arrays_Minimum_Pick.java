5d96de2ef6a5f522032ad9bb9eae3088e650b3b1
tomar <tomarankit78@gmail.com>
2026-01-07 22:19:15 +0530

    New Learning Java Programs Added

--- dsa_advance/array/Q_9_Arrays_Minimum_Pick.java
+++ dsa_advance/array/Q_9_Arrays_Minimum_Pick.java
@@ -6,19 +6,21 @@ public class Q_9_Arrays_Minimum_Pick {
         Return the difference between the maximum among all even
             numbers of nums and the minimum among all odd numbers in nums. */
 
-        int nums[]={5,17,100,1};
-        int max_even=Integer.MIN_VALUE;
-        int min_odd=Integer.MAX_VALUE;
+        long nums[]={5,17,100,1};
+        long max_even=Long.MIN_VALUE;
+        long min_odd=Long.MAX_VALUE;
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0){
+                if(nums[i]>max_even)
                 max_even=nums[i];
             }
             if(nums[i]%2!=0){
+                if(nums[i]<min_odd)
                     min_odd=nums[i];
             }
         }
-        int result=Math.abs(max_even)-Math.abs(min_odd);
+        long result=Math.abs(max_even)-Math.abs(min_odd);
               System.out.println("Even max : "+max_even);
               System.out.println("Odd min : "+min_odd);
               System.out.println("Difference is \n "+result);

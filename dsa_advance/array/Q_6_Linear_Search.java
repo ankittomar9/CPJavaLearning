71e3d4e52e091c1e73663590900f73b69bc56ede
tomar <tomarankit78@gmail.com>
2026-01-07 20:03:19 +0530

    New Learning Java Programs Added

--- dsa_advance/array/Q_6_Linear_Search.java
+++ dsa_advance/array/Q_6_Linear_Search.java
@@ -2,6 +2,23 @@ package dsa_advance.array;
 
 public class Q_6_Linear_Search {
     public static void main(String args[]){
+        int arr[]={10,99,5,44,32,41,62,75,22,26,35};
+        int target=62; 
+        boolean isFound=false;
+        //Linear Search 
+        int index=0; // if duplicate this will give last index of element found
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                index=i;
+                isFound=true;
+            }
+
+        }
+            if(isFound==true){
+                  System.out.println("Linear search \ntarget : "+target+" found at index : "+index);
+            }else{
+              System.out.println("Linear search \n target not exist");
+            }
         
     }
     

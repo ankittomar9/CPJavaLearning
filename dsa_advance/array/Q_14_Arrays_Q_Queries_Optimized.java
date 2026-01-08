b092a0e47d14ae5fddfa2e424515f3ce1a730723
tomar <tomarankit78@gmail.com>
2026-01-08 22:10:41 +0530

    New Learning Java Programs Added

--- dsa_advance/array/Q_14_Arrays_Q_Queries_Optimized.java
+++ dsa_advance/array/Q_14_Arrays_Q_Queries_Optimized.java
@@ -1,27 +1,22 @@
 package dsa_advance.array;
-
 import java.util.Arrays;
 public class Q_14_Arrays_Q_Queries_Optimized {
     public static void main(String args[]){
         int arr[]={10,20,10,5,-16};
           System.out.println("Original Array \n "+Arrays.toString(arr));
       
-          int prefix_arr[]=new int[arr.length];
+         int prefix_arr[]=new int[arr.length];
         prefix_arr[0]=arr[0];
         for(int i=1;i<arr.length;i++){
             prefix_arr[i]=prefix_arr[i-1]+arr[i];
         }
           System.out.println("Prefix Array \n "+Arrays.toString(prefix_arr));
-        
           int[][] queries = {
             {1, 3},   // Query 1: sum from index 1 to 3
             {0, 4},   // Query 2: sum from index 0 to 4 (Edge case s=0)
             {2, 2}    // Query 3: sum of just index 2
         };
-
-
-        //Query sum  s=1 ,e=3;
-        //int s=1;int e=3; 
+        //Query sum  s=1 ,e=3;        //int s=1;int e=3; 
         int sum=0;
         for(int i=0;i<queries.length ;i++){
             int s=queries[i][0];
@@ -34,8 +29,5 @@ public class Q_14_Arrays_Q_Queries_Optimized {
             }
            System.out.println("Range [" + s + ", " + e + "] Sum = " + sum);
         }
-
-       //   System.out.println("Sum of range from s to e : \n  "+sum);
-    }
-    
+    }    
 }

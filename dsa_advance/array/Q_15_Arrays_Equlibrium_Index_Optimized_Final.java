48837dfbd3f216cbe515c4ff58ca89593ddece86
tomar <tomarankit78@gmail.com>
2026-01-08 19:10:40 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_15_Arrays_Equlibrium_Index_Optimized_Final.java
@@ -0,0 +1,40 @@
+package dsa_advance.array;
+
+public class Q_15_Arrays_Equlibrium_Index_Optimized_Final {
+    public static void main(String args[]){
+         int nums[]={-7,1,5,2,-4,3,0};
+         //Optimize without prefix array 
+        int totalSum=0;
+        for(int i=0;i<nums.length;i++){
+            totalSum+=nums[i];
+        }
+        System.out.println(totalSum);
+
+        for(int i=0;i<nums.length;i++){
+            //Left Sum
+            int leftSum=0;
+            if(i==0){
+                leftSum=0;
+            }
+            else{
+                
+            }
+
+            //right Sum=0;
+            int rightSum=0;
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+    
+}

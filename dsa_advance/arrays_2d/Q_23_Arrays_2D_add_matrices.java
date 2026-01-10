4f38fd105b88fb467522476304562a3934f4fe5e
tomar <tomarankit78@gmail.com>
2026-01-10 22:29:33 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/arrays_2d/Q_23_Arrays_2D_add_matrices.java
@@ -0,0 +1,23 @@
+package dsa_advance.arrays_2d;
+
+import java.util.Arrays;
+
+public class Q_23_Arrays_2D_add_matrices {
+    public static void main(String args[]){
+        int arr1[][]={{1,2,3},{4,5,6},{7,8,9}};
+          int arr2[][]={{1,2,3},{4,5,6},{7,8,9}};
+            int n=arr1.length; int m=arr1[0].length;
+          //Note you can only add two matrices if they are of same dimesnions
+      //Have time ask question what if dimensions are not same
+          int resultant_mat[][]=new int[n][m];
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                resultant_mat[i][j]=arr1[i][j]+arr2[i][j];
+            }
+          }
+
+          System.out.println(Arrays.deepToString(resultant_mat));
+   
+        }
+    
+}

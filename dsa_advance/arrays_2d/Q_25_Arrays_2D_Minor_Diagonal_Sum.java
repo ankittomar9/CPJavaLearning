62b213e81a3fbe0b4fadfb0294c7c1138c012344
tomar <tomarankit78@gmail.com>
2026-01-10 22:39:43 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/arrays_2d/Q_25_Arrays_2D_Minor_Diagonal_Sum.java
@@ -0,0 +1,17 @@
+package dsa_advance.arrays_2d;
+
+public class Q_25_Arrays_2D_Minor_Diagonal_Sum {
+    public static void main(String args[]){
+         int arr[][]={{1,2,3},
+                     {4,5,6},
+                     {7,8,9}};
+
+        
+
+
+
+
+
+    }
+    
+}

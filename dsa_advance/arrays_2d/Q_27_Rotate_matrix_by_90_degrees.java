15bb056501d419ee3cded01dd897964061783a49
tomar <tomarankit78@gmail.com>
2026-01-11 20:02:20 +0530

    New Learning Java Programs Added

--- dsa_advance/arrays_2d/Q_27_Rotate_matrix_by_90_degrees.java
+++ dsa_advance/arrays_2d/Q_27_Rotate_matrix_by_90_degrees.java
@@ -1,25 +1,35 @@
 package dsa_advance.arrays_2d;
+
 import java.util.Arrays;
 
 public class Q_27_Rotate_matrix_by_90_degrees {
     public static void main(String args[]){
-     int arr[][]={{1,2,3},
-                {4,5,6},
-                 {7,8,9}};
-        int n=arr.length;int m=arr[0].length;             
-    // step 1 : To rotate first do matrix transpose
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<m;j++){
-            int temp=arr[i][j];
-            arr[i][j]=arr[j][i];
-            arr[j][i]=temp;
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int n=arr.length;int m=arr[0].length;
+        //step 1: Rotate a matrix clockwise 90 we need first matrix transpose
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
         }
-    }              
-    System.out.println(Arrays.deepToString(arr));
+        System.out.println(Arrays.deepToString(arr));
+       // Step 2: Then  we reverse the row to completely rotate it
+        
+        for(int i=0;i<n;i++){
+            int start=0; int end=n-1;
 
-    
-     System.out.println(Arrays.deepToString(arr));
-    
+            while(start<end){
+                int temp=arr[i][start];
+                arr[i][start]=arr[i][end];
+                arr[i][end]=temp;
+                start++;
+                end--;
+            }
+
+        }
+        System.out.println(Arrays.deepToString(arr));
     }
 
   

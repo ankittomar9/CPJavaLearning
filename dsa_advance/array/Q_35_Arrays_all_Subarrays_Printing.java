66d204e06e75cfe571066dd714efa5ecc0ef874c
tomar <tomarankit78@gmail.com>
2026-01-11 23:06:44 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_35_Arrays_all_Subarrays_Printing.java
@@ -0,0 +1,21 @@
+package dsa_advance.array;
+
+public class Q_35_Arrays_all_Subarrays_Printing {
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+
+        for(int i=0;i<arr.length;i++){ //Visit every element
+
+            for(int j=i;j<arr.length;j++){ // get starting and ending indexes of subarray
+                
+                for(int k=i;k<=j;k++){
+                    
+                    System.out.print(" "+arr[k]);
+              
+                }
+                System.out.print(" \n");
+            }
+        }
+    }
+    
+}

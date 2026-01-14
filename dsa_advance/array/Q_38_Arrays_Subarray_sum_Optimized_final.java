8826ced76d7be5cbe7f0fbfdf3e2472f4b8db5d7
tomar <tomarankit78@gmail.com>
2026-01-14 20:13:45 +0530

    New Learning Java Programs Added

--- dsa_advance/array/Q_38_Arrays_Subarray_sum_Optimized_final.java
+++ dsa_advance/array/Q_38_Arrays_Subarray_sum_Optimized_final.java
@@ -2,7 +2,8 @@ package dsa_advance.array;
 
 public class Q_38_Arrays_Subarray_sum_Optimized_final {
     public static void main(String args[]){
-        int arr[]={1,2,3,4,5};
+      //  int arr[]={1,2,3,4,5};
+        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
         int n=arr.length;
       
         for(int i=0;i<arr.length;i++){

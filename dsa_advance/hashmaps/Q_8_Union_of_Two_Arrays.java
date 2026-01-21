4cbf29ee98c0084a8245873d3ba2cbef9c936de8
tomar <tomarankit78@gmail.com>
2026-01-21 22:09:33 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/hashmaps/Q_8_Union_of_Two_Arrays.java
@@ -0,0 +1,23 @@
+package dsa_advance.hashmaps;
+import java.util.HashSet;
+public class Q_8_Union_of_Two_Arrays {
+    public static void main(String args[]){
+         int arr1[]={7,3,9};
+          int arr2[]={6,3,9,2,2,4};
+
+                HashSet<Integer> set=new HashSet<>();
+
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+        System.out.println(set);
+
+        System.out.println("Union Elements:");
+        for(int i=0;i<arr2.length;i++){
+           set.add(arr2[i]);
+        }
+
+        System.out.println("Union Size: " + set.size());
+        System.out.println("Union Elements: " + set);
+    } 
+}

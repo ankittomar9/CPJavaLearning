492d74a4a81abc5c528f8e21d95852176b0f48f7
tomar <tomarankit78@gmail.com>
2026-01-21 22:09:21 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/array/Q_47_Count_the_triplets_brute.java
@@ -0,0 +1,9 @@
+package dsa_advance.array;
+
+public class Q_47_Count_the_triplets_brute {
+    public static void main(String args[]){
+        //Count triplets where arr[i]<arr[j]<arr[k];
+        int arr[]={};
+    }
+    
+}

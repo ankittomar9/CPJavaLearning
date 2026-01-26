aedfab0994fd0a9d0f0c3da82d5e4e9a90bdcc6d
tomar <tomarankit78@gmail.com>
2026-01-26 18:40:01 +0530

    New Learning Java Programs Added

--- dsa_advance/hashmaps/Q_10_First_Repeating_Element.java
+++ dsa_advance/hashmaps/Q_10_First_Repeating_Element.java
@@ -8,12 +8,13 @@ public class Q_10_First_Repeating_Element {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         // Step 1: Build Frequency Map
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length;i++){
             if(map.containsKey(arr[i])) {
                 map.put(arr[i], map.get(arr[i]) + 1);
             } else {
                 map.put(arr[i], 1);
             }
+        }    
         // Step 2: Check for the first one with count > 1
         boolean found = false;
              for(int i=0;i<arr.length;i++){ 

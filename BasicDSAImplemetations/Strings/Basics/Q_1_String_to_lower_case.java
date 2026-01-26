75dc9b92769b499d931bfd21ee54d527b2c5c9d9
tomar <tomarankit78@gmail.com>
2026-01-26 18:39:46 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Strings/Basics/Q_1_String_to_lower_case.java
@@ -0,0 +1,27 @@
+package BasicDSAImplemetations.Strings.Basics;
+
+import java.util.Arrays;
+
+public class Q_1_String_to_lower_case {
+    public static void main(String args[]){
+       String s1 = "ANKIT_is_LEARNING";
+                System.out.println("Original Array String : \n"+s1);
+
+        char arr[]=s1.toCharArray();
+
+        for(int i=0;i<s1.length();i++){
+            if(arr[i]>=65 && arr[i]<=90){
+                  arr[i]=(char) (arr[i]+32);
+            }
+          
+        }   
+
+        String s2=new String(arr);
+        System.out.println("New Array String : \n"+s2);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
+//TC: O(n)
+//SC:O(n) we becuase we need an aux char array to run the conversion logic 
+// Can it be done in O(1) no String builder and char array takes same O(n) space

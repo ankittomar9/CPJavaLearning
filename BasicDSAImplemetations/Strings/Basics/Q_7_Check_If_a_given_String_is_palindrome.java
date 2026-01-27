8a41c6453e7521c75164f415bf950410dbd3008e
tomar <tomarankit78@gmail.com>
2026-01-27 22:54:01 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Strings/Basics/Q_7_Check_If_a_given_String_is_palindrome.java
@@ -0,0 +1,26 @@
+package BasicDSAImplemetations.Strings.Basics;
+
+public class Q_7_Check_If_a_given_String_is_palindrome {
+    public static void main(String args[]){
+        String s1="naman"; //String s1="aman"; for false case
+        if(s1==null || s1.length()==0){
+            System.out.println("String is null || or empty ");        }
+            
+         System.out.println("Original String : \n"+s1);
+        String reversed="";
+        for(int i=0;i<s1.length();i++){
+            reversed=reversed+s1.charAt(i);
+        }
+
+        System.out.println("Reversed String : \n"+reversed);
+
+  //don't do this s1==reversed it fails becuase  == compares primitive and object data types
+        if(s1.equals(reversed)){
+            System.out.println("Given String is Palindromic");
+        }else{
+             System.out.println("Given String is not Palindromic");
+        }
+
+    }
+    
+}

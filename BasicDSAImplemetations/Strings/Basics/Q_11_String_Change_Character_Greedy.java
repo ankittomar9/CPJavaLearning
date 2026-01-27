9d46b5ad18c19779dfabc2308ba21eaa0e900500
tomar <tomarankit78@gmail.com>
2026-01-27 22:54:13 +0530

    New Learning Java Programs Added

---
+++ BasicDSAImplemetations/Strings/Basics/Q_11_String_Change_Character_Greedy.java
@@ -0,0 +1,13 @@
+package BasicDSAImplemetations.Strings.Basics;
+public class Q_11_String_Change_Character_Greedy {
+    public static void main(String args[]){
+/*You are given a string A of size N consisting of lowercase alphabets.
+You can change at most B characters in the given string to any other lowercase
+ alphabet such that the number of distinct characters in the string is minimized.
+Find the minimum number of distinct characters in the resulting string. */
+
+
+
+    }
+    
+}

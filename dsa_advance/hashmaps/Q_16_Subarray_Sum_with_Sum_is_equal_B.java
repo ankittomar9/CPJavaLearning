65dd011f97453f5b37816b7ca7f83fa396d99ccb
tomar <tomarankit78@gmail.com>
2026-01-29 21:51:16 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/hashmaps/Q_16_Subarray_Sum_with_Sum_is_equal_B.java
@@ -0,0 +1,31 @@
+package dsa_advance.hashmaps;
+import java.util.HashMap;
+public class Q_16_Subarray_Sum_with_Sum_is_equal_B {
+    public static void main(String args[]){
+        int arr[]={10,2,-2,-20,10}; int B=-10;
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        map.put(0,-1);
+
+            int currentSum=0; 
+            boolean isFound=false;
+
+            for(int i=0;i<arr.length;i++){
+                currentSum=currentSum+arr[i];
+                // CurrentSum - OldSum = B
+                // OldSum = CurrentSum - B
+                int needed_Old_Sum = currentSum-B;
+
+                if(map.containsKey(needed_Old_Sum)){
+                    System.out.println("Subarray found from Index " + 
+                                   (map.get(needed_Old_Sum) + 1) + " to " + i);
+                isFound = true;
+                break;
+                }
+                map.put(currentSum, i);
+            }
+
+            if (!isFound) System.out.println("No subarray found");
+    }
+    
+}

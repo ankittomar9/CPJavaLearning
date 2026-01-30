599801c8f1cf32ad061e3f638299bd3313a7ab01
tomar <tomarankit78@gmail.com>
2026-01-30 22:10:31 +0530

    New Learning Java Programs Added

---
+++ dsa_advance/bitmanipulation/Q_4_Set_ith_Bit.java
@@ -0,0 +1,19 @@
+package dsa_advance.bitmanipulation;
+
+public class Q_4_Set_ith_Bit {
+    public static void main(String args[]){
+        /*Write a program that takes n and i,
+         and prints the result of Setting the bit. (changing the bit at ith location) */
+        int n=9;
+        int i=1;
+
+        int mask=1<<i;
+        int result= n | mask; // check dry run why we did OR here with mask
+        // 9 -> Binary(8421) -> 1001 
+        // 11 -> Binary(8421) -> 1011 which is  
+
+        System.out.println("Original numbner :"+n);
+        System.out.println("Modified number after setting :"+result);
+    }
+    
+}

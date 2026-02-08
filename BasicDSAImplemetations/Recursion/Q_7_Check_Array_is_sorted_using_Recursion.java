a0aab40306cf2b27b1fa82623d91a349b92c4373
tomar <tomarankit78@gmail.com>
2026-02-08 22:13:02 +0530

    New Learning Java Programs Added

--- BasicDSAImplemetations/Recursion/Q_7_Check_Array_is_sorted_using_Recursion.java
+++ BasicDSAImplemetations/Recursion/Q_7_Check_Array_is_sorted_using_Recursion.java
@@ -4,6 +4,7 @@ public class Q_7_Check_Array_is_sorted_using_Recursion {
     public static void main(String args[]){
         int arr[]={1,2,3,4};
         // int arr[]={1,3,4,2}; // to check the negative case
+    
         boolean isCheck=isArraySorted(arr,0);
 
         System.out.println("Is Array Sorted : \n" + isCheck);
